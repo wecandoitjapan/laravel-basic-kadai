56e6b657f7c5af7be98ce49710b2677e408292fc
wecandoitjapan <kazunori.fujimoto.1981@gmail.com>
2025-02-17 22:49:57 +0900

    課題　静的なページを表示しよう　初回

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/posts', function () {
+    return '投稿一覧';
 });
