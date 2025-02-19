288ea6d6787d0ea8241c741523464b275f97134f
wecandoitjapan <kazunori.fujimoto.1981@gmail.com>
2025-02-20 08:55:15 +0900

    クエリビルダを使ってデータを取得しよう 初回提出

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller{
     public function index() {
-        return view('posts.index');
-    }
+        // postsテーブルからすべてのデータを取得し、変数$postsに代入する
+        $posts = DB::table('posts')->get();
+
+        // 変数$productsをproducts/index.blade.phpファイルに渡す
+        return view('posts.index', compact('posts'));
+    }    
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,7 +8,24 @@
 </head>
 
 <body>
-   <h1>投稿一覧</h1>
+   <table>
+       <tr>
+           <th>ID</th>
+           <th>タイトル</th>
+           <th>コンテンツ</th>
+           <th>作成日時</th>
+           <th>更新日時</th>
+       </tr>
+       @foreach($posts as $post)
+           <tr>
+               <td>{{ $post->id }}</td>
+               <td>{{ $post->title }}</td>
+               <td>{{ $post->content }}</td>
+               <td>{{ $post->created_at }}</td>
+               <td>{{ $post->updated_at }}</td>
+           </tr>            
+       @endforeach       
+   </table>
 </body>
 
 </html>
